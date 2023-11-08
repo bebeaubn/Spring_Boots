0747f41bd5086f5a319ff9f4b74dbe569a5de501
bebeaubn <yebin0213@naver.com>
2023-11-08 17:14:56 +0900

    수업 소스

--- src/main/java/org/koreait/restcontrollers/ApiMemberController.java
+++ src/main/java/org/koreait/restcontrollers/ApiMemberController.java
@@ -1,7 +1,25 @@
 package org.koreait.restcontrollers;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.entities.Member;
+import org.koreait.repositories.MemberRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
 public class ApiMemberController {
+
+    private final MemberRepository repository;
+
+    @GetMapping("{/userId}")
+    public Member info(@PathVariable String userId){
+        Member member = repository.findByUserId(userId);
+
+        return member;
+
+    }
 }
--- src/main/java/org/koreait/schedules/PostStat.java
+++ src/main/java/org/koreait/schedules/PostStat.java
@@ -16,7 +16,7 @@ public class PostStat {
     //@Scheduled(cron = "*/5 * * * * *")   //5초마다
 
     //@Scheduled(fixedDelay = 5, timeUnit = TimeUnit.SECONDS)  //5초 고정간격으로 실행
-    @Scheduled(initialDelay = 5, timeUnit = TimeUnit.SECONDS)  // 작업 시작전 5초 대기
+    //@Scheduled(initialDelay = 5, timeUnit = TimeUnit.SECONDS)  // 작업 시작전 5초 대기
     public void process(){
 
         log.info("게시글 통계 작업 수행....");
